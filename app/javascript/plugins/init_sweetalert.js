3bd5884875979b557e84dff16a4beb95f4b9dc05
Pierre Bedouret <pierrebedouret@hotmail.com>
2019-11-15 12:26:47 +0100

    Update sweetAlert feature = Done

--- app/javascript/plugins/init_sweetalert.js
+++ app/javascript/plugins/init_sweetalert.js
@@ -3,26 +3,22 @@ import swal from 'sweetalert';
 
 const initSweetalert = (selector, options = {}, callback = () => {}) => {
   const swalButtons = document.querySelectorAll('.delete-dose');
-  console.log(swalButtons);
+
   if (swalButtons) { // protect other pages
     swalButtons.forEach((swalButton) => {
       swalButton.addEventListener('click', (event) => {
-        console.log(event);
-        console.log(event.currentTarget);
         const id = event.currentTarget.id;
-        console.log(id);
+        const name = event.currentTarget.dataset.name
         options = {
                     title: "WARNING",
-                    text: `Do you realy want to delete ${id}?`,
+                    text: `Do you realy want to delete ${name}?`,
                     dangerMode: true,
                     buttons: true,
                     icon: "warning"
                   };
         swal(options).then((value) => {
-          console.log(value);
-          console.log(id);
           if (value) {
-            const link = document.querySelector('#delete-link');
+            const link = document.querySelector(`#delete-link-${id}`);
             link.click();
           }
         });
